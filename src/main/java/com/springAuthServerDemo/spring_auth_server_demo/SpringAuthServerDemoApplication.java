cbef7dcfa866b3edbba69b52edb19027d6e29afc
marwhals <marjan.mubarok2012@gmail.com>
2025-04-18 21:33:50 +0100

    Set up project

---
+++ src/main/java/com/springAuthServerDemo/spring_auth_server_demo/SpringAuthServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springAuthServerDemo.spring_auth_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springAuthServerDemo/spring_auth_server_demo/SpringAuthServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAuthServerDemo.spring_auth_server_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
